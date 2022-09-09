f64322f3bad69063ae5d5419be4de732c24b25f9
Henk Rusting <henk.rusting@gmail.com>
2022-09-09 21:02:39 +0200

    Added ValueError check cleanup

--- code/practice_henk/exercise_1.py
+++ code/practice_henk/exercise_1.py
@@ -1,16 +1,27 @@
 # Excercise 1 Calculate Moment on a beam with a distributed load
 # Henk Rusting 9717445
 
-
-# Get user input (Length and distributed load)
-l = float(input("Enter Beam Length (m): "))
-q = float(input("Enter Distributed Load (kN): "))
-
-# Calculate moment using function
+# Define moment caluculation function
 def moment(q, l):
-    m = round((q*(l**2))/8,1)
+    m = round((q*(pow(l, 2)))/8,1)
 
     return str(m) + " kNm"
+
+# Get user input (Length of beam). Including Check for valid input.
+while True:
+    try:
+        l = float(input("Enter Beam Length (m): "))
+        break
+    except ValueError:
+         print("Not a valid number.  Try again...")
+
+# Get user input (Distributed load). Including Check for valid input.
+while True:
+    try:
+        q = float(input("Enter Distributed Load (kN): "))
+        break
+    except ValueError:
+         print("Not a valid number.  Try again...")
 
 # Call function
 result = moment(q, l)
