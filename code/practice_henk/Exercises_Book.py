156949a3c990803346e8ec697a70607c05bf112e
Henk Rusting <henk.rusting@gmail.com>
2022-09-10 19:41:38 +0200

    Excercises Chapter 1 Book

---
+++ code/practice_henk/Exercises_Book.py
@@ -0,0 +1,23 @@
+# Excercises Chapter 1 Book A Practical Introduction to Python Programming
+# Henk Rusting 9717445
+
+
+#Excercise 1
+for i in range(4):
+    print('********************')
+print(' ')
+
+#Excercise 2
+print('********************')
+for i in range(2):
+    print("*                  *")
+print('********************')
+print(' ')
+
+#Excercise 3
+
+for i in range(4):
+    number_of_stars = i + 1
+    print('*'*number_of_stars)
+
+
