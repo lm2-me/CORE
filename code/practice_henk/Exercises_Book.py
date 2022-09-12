0648c7018a47e289c214f3723d91e894cc85e4e0
Henk Rusting <henk.rusting@gmail.com>
2022-09-12 15:06:01 +0200

    Added Excercise 6&7

--- code/practice_henk/Exercises_Book.py
+++ code/practice_henk/Exercises_Book.py
@@ -1,38 +1,73 @@
-# Excercises Chapter 1 Book A Practical Introduction to Python Programming
-# Henk Rusting 9717445
+# # Excercises Chapter 1 Book A Practical Introduction to Python Programming
+# # Henk Rusting 9717445
 
 
-#Excercise 1
-from sys import float_repr_style
+# #Excercise 1
+# from sys import float_repr_style
 
 
-for i in range(4):
-    print('********************')
-print(' ')
+# for i in range(4):
+#     print('********************')
+# print(' ')
 
-#Excercise 2
-print('********************')
-for i in range(2):
-    print("*                  *")
-print('********************')
-print(' ')
+# #Excercise 2
+# print('********************')
+# for i in range(2):
+#     print("*                  *")
+# print('********************')
+# print(' ')
 
-#Excercise 3
+# #Excercise 3
 
-for i in range(4):
-    number_of_stars = i + 1
-    print('*'*number_of_stars)
-print(' ')
+# for i in range(4):
+#     number_of_stars = i + 1
+#     print('*'*number_of_stars)
+# print(' ')
 
-#Excercise 4
+# #Excercise 4
 
-print((512-282)/((47*48)+5))
-print(' ')
+# print((512-282)/((47*48)+5))
+# print(' ')
 
-#Excercise 5
+# #Excercise 5
 
-try:
-    temp = float(input("Enter a number: "))
-    print("The square of ", temp, " is ", pow (temp, 2), ".", sep='')
-except ValueError:
-         print("Not a valid number.  Try again...")
+# try:
+#     temp = float(input("Enter a number: "))
+#     print("The square of ", temp, " is ", pow (temp, 2), ".", sep='')
+# except ValueError:
+#          print("Not a valid number.  Try again...")
+# print(' ')
+
+# #Exercise 6
+# while True:
+#     try:
+#         x = float(input("Enter a number: "))
+#         break
+#     except ValueError:
+#         print("Not a valid number.  Try again...")
+# print(x ,2*x ,3*x ,4*x ,5*x , sep='---')
+# print(' ')
+
+#Exercise 7
+
+# Define function weight kilograms to pounds
+def weigth_in_pounds(weigth):
+    lbs = round(kg*2.2, 1)
+    return str(lbs) + " is your weigth in Pounds."
+
+# # Get user input (weigth in kg). Check input
+# while True:
+#     try:
+#         kg = float(input("Enter your weigth (kg): "))
+#         break
+#     except ValueError:
+#         print("Not a valid number.  Try again...")
+# print (kg)
+
+# # Call function
+# result = weigth_in_pounds(kg)
+# print(result)
+
+# print(' ')
+
+
