1fcf4c350759b77058808090d72318acee1e81f2
Job de Vogel <jobdvogel@gmail.com>
2022-09-16 18:30:21 +0200

    init

---
+++ code/utils/shortest_path.py
@@ -0,0 +1,11 @@
+# My own implementation for shortest path
+
+# Single_shortest path (based on taxicab, but using a* algorithm)
+def single_shortest_path():
+    pass
+
+# Convert to multicore process based on osmnx method
+def multicore_shortest_path():
+    pass
+
+# Make a difference between shortest distance and the actual node path
