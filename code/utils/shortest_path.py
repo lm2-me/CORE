a1725cbb431e0fa25d3a4f6a20a9ac76731983c2
Job de Vogel <jobdvogel@gmail.com>
2022-09-21 20:24:49 +0200

    Updated (WIP)

--- code/utils/shortest_path.py
+++ code/utils/shortest_path.py
@@ -1,17 +1,16 @@
-import os
-import osmnx as ox
 import networkx as nx
-import time
 import matplotlib.pyplot as plt
 
 from shapely.geometry import Point
 from shapely.geometry import LineString
 from shapely.ops import substring
 
-from osmnx import load_graphml
 from osmnx.distance import nearest_edges
 from osmnx.distance import great_circle_vec
 from osmnx.utils_graph import get_route_edge_attributes
+
+import multiprocessing as mp
+from functools import partial
 
 from pyproj import Transformer
 
@@ -25,32 +24,59 @@ from pyproj import Transformer
     - Different shortest path algorithms can now be used, such as
     astar, dijkstra, bellman-ford.
     - A specific weight can be used to calculate shortest paths,
-    instead of only 'lenght', such as travel_time.
+    instead of only 'length', such as travel_time.
     - The nearest_edge calculation has been moved to another
     function and optimized.
 '''
 
+'''
+PROBLEMS:
+- Length not correctly calculated due to coordinate system
+- Linestring difference with taxicab
+
+'''
+
 # Transform coordinates from 'sphere'(globe) to 'surface (map)
-def transform_coordinates(lat, lon): 
+# Expects coordinates in (lat, lon) e.g. (51.99274, 4.35108)
+def transform_coordinates(coordinate: tuple or list):
+    # # WGS 84 -- WGS84 World Geodetic System 1984, used in GPS
+    # # More info at https://gist.github.com/keum/7441007
+    
     # From epsg:4326 to epsg:3857
     transformer = Transformer.from_crs("epsg:4326", "epsg:3857")
-    x, y = transformer.transform(lat, lon)
+    
+    if isinstance(coordinate, list): 
+        result = []
+        for coord in coordinate:
+            lat, lon = coord
+            x, y = transformer.transform(lat, lon)
+            result.append((y, x))
 
-    return (y, x)
+            if lat < lon:
+                print('WARNING: latitude and longitude probably in wrong order in tuple! (Netherlands)')
+    elif isinstance(coordinate, tuple):
+        lat, lon = coordinate
 
+        x, y = transformer.transform(lat, lon)
+        result = (y, x)
+
+        if lat < lon:
+            print('WARNING: latitude and longitude probably in wrong order in tuple! (Netherlands)')
+    else:
+        raise TypeError('Inputs should be Tuple or List')
+
+    return result
+
+# Expects results in same coordinate system as graph.
+# Make sure you first transform coordinates and project graph.
 def find_nearest_edge(graph, orig_yx, dest_yx):
     """
     Optimize using ox.distance.nearest_edges with interpolate, see documentation
     """
-    # # WGS 84 -- WGS84 World Geodetic System 1984, used in GPS
-    # # More info at https://gist.github.com/keum/7441007
-    orig_yx = transform_coordinates(orig_yx[0], orig_yx[1])
-    dest_yx = transform_coordinates(dest_yx[0], dest_yx[1])
-
     # # # Find the closest edges to origin and destination
     orig_edge = nearest_edges(graph, orig_yx[1], orig_yx[0]) 
     dest_edge = nearest_edges(graph, dest_yx[1], dest_yx[0])
-    
+
     return orig_edge, dest_edge
 
 # Calculate the total weight of the path, including partial edges
@@ -66,10 +92,16 @@ def _compute_route_weight(graph, route, weight, ls_orig, ls_dest, orig_edge, des
     if type(ls_orig) == LineString:
         x, y = zip(*ls_orig.coords)
         
+        """
+        PROBLEM
+
+        great_circle_vec should be calculated with latitudes and longitudes!
+        """
+        
         partial_orig_edge_length = 0
         for i in range(0, len(x)-1):
             partial_orig_edge_length += great_circle_vec(y[i], x[i], y[i+1], x[i+1])
-        
+
         partial_orig_factor = partial_orig_edge_length / orig_edge_length
         partial_orig_edge_weight = partial_orig_factor * graph.edges[orig_edge][weight]
     
@@ -135,7 +167,6 @@ def _single_shortest_path(G, orig_yx, dest_yx, orig_edge, dest_edge,
     method='dijkstra', 
     weight='length',
     return_path=False):  
-        
     # Check if on samen line, if so, use simplified calculation
     if orig_edge == dest_edge:
         # Revert x and y coordinates        
@@ -236,43 +267,49 @@ def _single_shortest_path(G, orig_yx, dest_yx, orig_edge, dest_edge,
     # parallel computation does not have to store the route in memory every path
     if not return_path:
         nx_route = None
-        orig_partial_edge = None
-        dest_partial_edge = None
 
-    return route_weight, nx_route, orig_partial_edge_1, dest_partial_edge
+    return route_weight, nx_route, orig_partial_edge, dest_partial_edge
 
 # Convert to multicore process based on osmnx method
-def multicore_shortest_path():
-    pass
+# This code is based on the shortest_path method
+# from OSMnx
+def multicore_shortest_path(graph, orig, dest, orig_edge, dest_edge, method='dijkstra', weight='travel_time', return_path=False, cpus=1):
+    if isinstance(orig, tuple) or isinstance(dest, tuple):
+        raise TypeError('Orig and dest should be lists, not tuples.')
+    
+    if not isinstance(orig_edge, list) or not isinstance(dest_edge, list):
+        raise TypeError('Orig_edge and dest_edge should be lists, not tuples.')
+    
+    if not (hasattr(orig, "__iter__") or hasattr(dest, "__iter__")):
+        # If neither orig nor dest is iterable, just return the shortest path
+        return _single_shortest_path(graph, orig, dest, method=method, weight=weight, return_path=return_path)
+    elif hasattr(orig, "__iter__") and hasattr(dest, "__iter__"):
+        # If both orig and dest are iterables ensure they have same lengths
+        if len(orig) != len(dest):  # pragma: no cover
+            raise ValueError("Orig, dest, orig_edge and dest_edge must contain same number of elements")
+        
+        # Figure out how many cpu cores are available
+        if cpus is None:
+            cpus = mp.cpu_count()
+        cpus = min(cpus, mp.cpu_count())
+        print(f"Solving {max(len(orig), len(dest))} paths using {method} algorithm with {cpus} CPUs...")
 
-print('loading...')
-path = os.path.abspath(__file__ + "/../../../data/Delft.osm")
-graph = load_graphml(filepath=path)
-graph = ox.speed.add_edge_speeds(graph)
-graph = ox.distance.add_edge_lengths(graph)
-graph = ox.speed.add_edge_travel_times(graph)
+        if cpus == 1:
+            # if single-threading, calculate each shortest path one at a time
+            # Return route_weight, route, partial_edge_1 and partial_edge_2
+            result = [_single_shortest_path(graph, o, d, ls_o, ls_d, method=method, weight=weight, return_path=return_path) for o, d, ls_o, ls_d in zip(orig, dest, orig_edge, dest_edge)]
+        else:
+            print("WARNING: Make sure you put the multicore_shortest_path function in a 'if __name__ == '__main__' statement!")
+            # If multi-threading, calculate shortest paths in parallel
+            args = ((graph, o, d, ls_o, ls_d) for o, d, ls_o, ls_d in zip(orig, dest, orig_edge, dest_edge))
+            pool = mp.Pool(cpus)
 
-graph = ox.project_graph(graph, to_crs="EPSG:3857")
-
-orig = (51.99274, 4.35108)
-dest = (52.02184, 4.37690)
-
-start = time.time()
-orig_edge, dest_edge = find_nearest_edge(graph, orig, dest)
-end = time.time()
-print(end-start)
-
-start = time.time()
-# Returns the nodes of the graph, orig edge and dest edge
-data = _single_shortest_path(graph, orig, dest, orig_edge, dest_edge, weight='travel_time', method = 'astar')
-end = time.time()
-print(end-start)
-
-"""
-To do:
-    - Implement route_weight function \/
-    - If nodes are not required, do not return them (better for parallel) \/
-    - Make different function for nearest edges orig and dest \/
-    - Make transform work with lists
-    - Implement multicore
-"""
+            # Add kwargs using partial method
+            sma = pool.starmap_async(partial(_single_shortest_path, method=method, weight=weight, return_path=return_path), args)
+            result = sma.get()
+            pool.close()
+            pool.join()
+        
+        return result
+    else:  # pragma: no cover
+        raise ValueError("Please check shortest path inputs.")
