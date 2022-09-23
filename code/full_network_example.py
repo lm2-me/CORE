5e5d728bf6b1189c5928c484b8fa0da4c666fe9a
Job de Vogel <jobdvogel@gmail.com>
2022-09-23 14:43:21 +0200

    Finished Jobs work

--- code/full_network_example.py
+++ code/full_network_example.py
@@ -9,55 +9,57 @@ from utils.multicore_nearest_edges import multicore_nearest_edge
 ImportError? Move this file to code folder TEMPORATILY
 '''
 
-'''
+''' --- CityNetwork EXAMPLE ---
+Developed by Job de Vogel
+
 This file is an example of how to use the 
 Delft_network script and the shortest_path.
+
+It includes references to:
+    - CityNetwork (Job de Vogel)
+    - multicore_shortest_path (Nathan A. Rooy, Job de Vogel)
+    - multicore_nearest_edges (gboeing, Job de Vogel)
 '''
 
-def plot(graph, route, orig, dest, route_color='grey'):
-    fig, ax = tc.plot.plot_graph_route(
-    graph, route, figsize=(10,10), show=False, close=False, route_color=route_color)
-
-    ax.scatter(orig[1], orig[0],
-    color='lime', marker='x', s=100, label='orig-point')
-
-    ax.scatter(dest[1], dest[0],
-        color='red', marker='x', s=100, label='dest-point')
-
 @timer_decorator
-def main():   
-    # Initialize CityNetwork object
-    Delft = CityNetwork('Delft_bike', [52.03, 51.96, 4.4, 4.3], 'walk')
+def main(): 
+    ''' --- GENERATE NETWORK ---
+    Generate a new network using the functions in the CityNetwork class. If a network already has been generated and stored in the data folder, skip this part and continue with PREPARE NETWORK. '''
+
+    # # Initialize CityNetwork object
+    # Delft = CityNetwork('Delft_bike', [52.03, 51.96, 4.4, 4.3], 'bike')
     
-    # Load osm from local or online file
-    Delft.load_osm_graph('data/Delft_walk.osm')
+    # # Load osm from local or online file
+    # Delft.load_osm_graph('data/Delft_bike.osm')
     
-    # Add speeds, lengths and distances to graph
-    Delft.add_rel_attributes()
+    # # Add speeds, lengths and distances to graph
+    # Delft.add_rel_attributes()
 
-    # Project graph
-    Delft.project_graph()
-    # Delft.plot()
+    # # Project graph
+    # Delft.project_graph()
+    # # Delft.plot()
 
-    # Calculate dataframes of nodes and edges
-    Delft.convert_graph_edges_to_df()
-    Delft.convert_graph_nodes_to_df()
+    # # Calculate dataframes of nodes and edges
+    # Delft.convert_graph_edges_to_df()
+    # Delft.convert_graph_nodes_to_df()
 
-    '''
-    Before continuing, you will have to decide what speeds
-    you are actually using on your paths. OSM assumes the
-    fastest allowed speed, e.g. 50 km/h on a bike...
-    '''
+    # '''
+    # Before continuing, you will have to decide what speeds
+    # you are actually using on your paths. OSM assumes the
+    # fastest allowed speed, e.g. 50 km/h on a bike...
+    # '''
 
-    # # Save Pickle file
-    Delft.save_graph('Delft_walk')
-    print('------------------------------------')
+    # # # Save Pickle file
+    # Delft.save_graph('Delft_bike')
+    # print('------------------------------------')
 
-    """
-    Load network from pickle file
-    """
+
+    ''' --- PREPARE NETWORK ---
+    Load the network from .pkl file. Transform the origin and destination to the unprojected space.'''
+    _multiply = 100
+
     # Load the Delft CityNetwork class object
-    #Delft = CityNetwork.load_graph('Delft')
+    Delft = CityNetwork.load_graph('Delft_bike')
 
     # Specify the coordinates for origin and destination
     orig = (51.99274, 4.35108)
@@ -72,34 +74,76 @@ def main():
     dest_yx_transf = transform_coordinates(dest)
 
     # Temporary overwrite, remove if you want to use coordinates
-    orig_yx_transf = (6803701, 484081)
-    dest_yx_transf = (6794547, 483281)
+    # It is easier to plot a graph and select manual points with 
+    # the unprojected coordinates.
+    orig_yx_transf = [(6803701, 484081), (6804701, 484281)]
+    dest_yx_transf = [(6800000, 480000), (6794000, 488700)]
     
-    # Find the nearest edges between the graph of Delft and origin and destination
-    # Recommended to combine all orig_yx_transf and dest_yx_transf into one list
-    # and then only run multicore_nearest_edge once.
-    # The script is currently optimized for a great amount of orig_edges and dest_edges
-    # that need to be calculated.
-    print('Finding origin and destination edges...')
-    orig_edge = multicore_nearest_edge(Delft.graph, orig_yx_transf[1], orig_yx_transf[0], 10, cpus=1)[0]
-    dest_edge = multicore_nearest_edge(Delft.graph, dest_yx_transf[1], dest_yx_transf[0], 10, cpus=1)[0]
-    
-    # EXAMPLE
-    path = multicore_shortest_path(graph, [orig_yx_transf], [dest_yx_transf], [orig_edge], [dest_edge], weight='travel_time', method = 'dijkstra', return_path=True, cpus=1)
-    print(path)
 
-    # Print the route from the Delft dataframe
+    ''' --- MULTICORE NEAREST EDGE COMPUTATION ---
+    Find the nearest edges between the graph of Delft and origin and destination. This edge is used to figure out the shortest route in the beginning and end of the path. The origin and destination coordinates are combined so that the time consuming interpolation of the graph only takes place once. Afterwards, the found edges are split again in origin and destination.
+
+    It may be usefull to save the CityNetwork again after computing, using Delft.save_graph('...'), so that you can skip calculating the nearest edges. Remember however that the algorithm does not check if the coordinate inputs are the same as in the saved pickle file. ONLY do this if you are planning to reuse the nearest edges.
+    The nearest edges from an earlier saved CityNetwork object can be removed using:
+    
+    Delft.ne = None
+    Delft.save_graph('...')
+    '''
+    print('------------------------------------')
+
+    # Remove existing nearest_edges in graph, proceed carefully:
+    Delft.ne = None
+
+    x = []
+    y = []
+
+    for orig in orig_yx_transf:
+        x.append(orig[1])
+        y.append(orig[0])
+    
+    for dest in dest_yx_transf:
+        x.append(dest[1])
+        y.append(dest[0])
+
+    # Find the nearest edges using multicore processing. If the CityNetwork class
+    # already has nearest edges stored, it skips the computation.
+    print('Finding origin and destination edges...')
+    edges = Delft.nearest_edges(x, y, 10, cpus=1)
+
+    # Save graph with edges, proceed carefully:
+    Delft.save_graph('Delft_bike')
+
+    # Split the found edges in origins and destinations (in half)
+    orig_edge = edges[:len(edges)//2]
+    dest_edge = edges[len(edges)//2:]
+
+    ''' --- MULTICORE SHORTEST PATH COMPUTATION ---
+    Compute the shortest path between origin and destination, taking in account the position of the start and end in relation to the nearest edges. Input of orig, dest, orig_edge and dest_edge will interally be converted to a list, if inputed as tuples. '''
+    print('------------------------------------')
+    paths = Delft.shortest_paths(orig_yx_transf, dest_yx_transf, orig_edge, dest_edge, weight='travel_time', method='dijkstra', return_path=True, cpus=1, _multiply=1)
+
+    '''' --- PRINT RESULTS DATAFRAME---
+    Although the paths have been found, you may want to see the results in a dataframe.'''
+    print('------------------------------------')
+
+    # Select a specific path:
+    path = paths[0]
+
     # Get information about the speeds, travel_time, road_names etc.
     edges = []
-    for i, node in enumerate(path[0][1][:-1]):
-        edges.append((node, path[0][1][i+1], 0))
+    for i, node in enumerate(path[1][:-1]):
+        edges.append((node, path[1][i+1], 0))
 
-    print(Delft.graph_edges_df.loc[edges, ['name', 'length', 'speed_kph']])
-    #print(Delft.graph_edges_df.loc[edges, :])
+    # Specify which attributes to show
+    # Not sure which attributes are available? Use Delft.get_edge_attribute_types()
+    df = Delft.graph_edges_df.loc[edges, ['name', 'length', 'speed_kph', 'travel_time']]
+    print(df)
     
-    # PLOT RESULTS
-    plot(graph, path[0], orig_yx_transf, dest_yx_transf, route_color='red')
-    plt.legend()
+    ''' --- PLOT RESULTS ---
+    Plot one or multiple paths using the CityNetwork plot functions. Advantage of this function is the ability to print multiple routes instead of one, including the linestrings calculated using the taxicab method.
+    
+    If you need different color and size settings for the plot, change them in the CityNetwork class (on top of class code)'''
+    fig, ax = Delft.plot(paths, orig_yx_transf, dest_yx_transf, save=True)
     plt.show()
 
 if __name__ == '__main__':
--- code/network_delft.py
+++ code/network_delft.py
@@ -2,6 +2,10 @@
 CityNetwork class which builds network from OSM
 and calculates shortest paths
 
+By: 
+Job de Vogel, TU Delft (framework, street data and shortest paths)
+Jirri van den Bos, TU Delft (housing)
+
 Classes:
     - CityNetwork
 
@@ -17,6 +21,7 @@ Classes:
         - get_edge_osmids: get all osm id's from edges
         - save_graph: save graph to pickle file
         - load_graph: load graph from pickle file
+        - nearest_edges: calculate nearest edges to coordinate
         - shortest_paths: calculate shortest paths between nodes or coordinates
         - plot: plot city graph without routes
 
@@ -35,10 +40,14 @@ Also combine method with taxicab procedure
 """
 
 import osmnx as ox
-import taxicab as tc
 import os.path
 import pickle
 import time
+
+from shapely.geometry import LineString
+
+from utils.multicore_shortest_path import multicore_shortest_path
+from utils.multicore_nearest_edges import multicore_nearest_edge
 
 class CityNetwork():
     # Plot settings
@@ -46,8 +55,11 @@ class CityNetwork():
     bgcolor = 'white'
     edge_color = 'gray'
     node_color = 'black'
-    route_linewidth = 1
+    edge_linewidth = 1
     node_size=3
+    route_color = 'darkorange'
+    route_width = 3
+    cross_color = 'red'
 
     def __init__(self, name: str, coordinates: list, transport_type, graph = None, graph_edges_df = None, graph_nodes_df = None):
         self.name = name
@@ -56,6 +68,7 @@ class CityNetwork():
         self.graph = graph
         self.graph_edges_df = graph_edges_df
         self.graph_nodes_df = graph_nodes_df
+        self.ne = None
 
     def __repr__(self):
         return "<CityNetwork object of {}>".format(self.name)
@@ -145,14 +158,94 @@ class CityNetwork():
         with open(path, 'wb') as file:
             pickle.dump(self, file, pickle.HIGHEST_PROTOCOL)
 
-    def shortest_paths(self, orig: int or list or tuple, dest: int or list or tuple, weight = 'travel_distance', from_coordinates = False, plot=False):
-        print('Calculating paths...')
-        pass
+    def nearest_edges(self, x, y, interpolate, cpus=None, _multiply=1):
+        if self.ne is None:
+            # If _multiply is given, multiply the number of samples
+            x *= _multiply
+            y *= _multiply
+
+            if len(x) != len(y):
+                raise ValueError('Please make sure x and y have the same length.') 
+
+            self.ne = multicore_nearest_edge(self.graph, x, y, interpolate, cpus=cpus)
+            return self.ne
+        else:
+            return self.ne
+
+    def shortest_paths(self, orig, dest, orig_edge, dest_edge, weight='travel_time', method='dijkstra', return_path=False, cpus=None, _multiply=1):
+        # Check if all inputs are lists, required for multicore calculation
+        if isinstance(orig, tuple):
+            orig = [orig]
+        if isinstance(dest, tuple):
+            dest = [dest]
+        if not isinstance(orig_edge, list):
+            orig_edge = [orig_edge]
+        if not isinstance(dest_edge, list):
+            dest_edge = [dest_edge]
+        
+        # If _multiply is given, multiply the number of samples
+        orig *= _multiply
+        dest *= _multiply
+        orig_edge *= _multiply
+        dest_edge *= _multiply
+
+        # Check if inputs have same length
+        if not (len(orig) == len(dest) == len(orig_edge) == len(dest_edge)):
+            raise ValueError('Please make sure orig, dest, orig_edge and dest_edge have the same length.') 
+        
+        # Execute in multicore
+        # If only one item, multicore will fall back on just single operation
+        paths = multicore_shortest_path(self.graph, orig, dest, orig_edge, dest_edge, weight=weight, method=method, return_path=return_path, cpus=cpus)
+
+        return paths
 
     # Plot the map without any routes
-    def plot(self):
-        print("Loading plot...")
-        ox.plot_graph(self.graph, figsize=self.figsize, bgcolor=self.bgcolor, edge_color=self.edge_color, node_color=self.node_color, node_size=self.node_size)
+    # Copy from osmnx and taxicab, slightly changed to work for multiple routes
+    def plot(self, routes, origins, destinations, save=False):
+        fig, ax = ox.plot_graph(self.graph, show=False, save=False, close=False,
+            figsize = self.figsize,
+            bgcolor = self.bgcolor,
+            edge_color = self.edge_color,
+            node_color = self.node_color,
+            edge_linewidth = self.edge_linewidth,
+            node_size = self.node_size)
+
+        for route, orig, dest in zip(routes, origins, destinations):
+            weight, route_nodes, ls_orig, ls_dest = route
+
+            x, y  = [], []
+            for u, v in zip(route_nodes[:-1], route_nodes[1:]):
+                # if there are parallel edges, select the shortest in length
+                data = min(self.graph.get_edge_data(u, v).values(), key=lambda d: d["length"])
+                if "geometry" in data:
+                    # if geometry attribute exists, add all its coords to list
+                    xs, ys = data["geometry"].xy
+                    x.extend(xs)
+                    y.extend(ys)
+                else:
+                    # otherwise, the edge is a straight line from node to node
+                    x.extend((self.graph.nodes[u]["x"], self.graph.nodes[v]["x"]))
+                    y.extend((self.graph.nodes[u]["y"], self.graph.nodes[v]["y"]))
+            ax.plot(x, y, c=self.route_color, lw=self.route_width)
+            
+            x, y = zip(*ls_orig.coords)
+            ax.plot(x, y, c=self.route_color, lw=self.route_width)
+
+            x, y = zip(*ls_dest.coords)
+            ax.plot(x, y, c=self.route_color, lw=self.route_width)
+
+            ax.scatter(orig[1], orig[0],
+                color=self.cross_color, marker='x', s=100, label='orig-point')
+            
+            ax.scatter(dest[1], dest[0],
+                color=self.cross_color, marker='x', s=100, label='orig-point')
+
+        fig, ax = ox.plot._save_and_show(fig, ax)
+
+        if save:
+            fig.savefig(f'data/plot_pngs/plot_{time.time()}.png')
+
+        return fig, ax
 
     # Loading the graph as a pickle file avoids having to recalculate
     # attributes such as speed, length etc. and is way faster
@@ -179,36 +272,3 @@ def timer_decorator(func):
         print('Executed {} fuction in {}s'.format(func.__name__, (round(end-start,2))))
         return func
     return wrapper
-
-@timer_decorator
-def main():
-    # """
-    # Initialize network if not already saved as pickle file:
-    # """
-    # # # # Initialize CityNetwork object
-    # Delft = CityNetwork('Delft', [52.03, 51.96, 4.4, 4.3])
-    
-    # # # # Load osm from local or online file
-    # Delft.load_osm_graph('data/Delft.osm')
-    
-    # # # # Add speeds, lengths and distances to graph
-    # Delft.add_rel_attributes()
-
-    # # # # Project graph
-    # Delft.project_graph()
-    # # # Delft.plot()
-
-    # # # # Calculate dataframes of nodes and edges
-    # Delft.convert_graph_edges_to_df()
-    # Delft.convert_graph_nodes_to_df()
-
-    # # # Save Pickle file
-    # Delft.save_graph('Delft')
-
-    """
-    Load network from pickle file
-    """
-    Delft = CityNetwork.load_graph('Delft')
-
-if __name__ == '__main__':
-    main()
