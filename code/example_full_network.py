0d3a41fa90b9b21eb3c4ccad5fbf3c9a3c327b3e
Job de Vogel <jobdvogel@gmail.com>
2022-09-26 21:16:20 +0200

    Added extra comments

--- code/example_full_network.py
+++ code/example_full_network.py
@@ -37,52 +37,54 @@ def main():
 
 
     ''' --- GENERATE NETWORK ---
-    Generate a new network using the functions in the CityNetwork class. If a network already has been generated and stored in the data folder, skip this part and continue with PREPARE NETWORK. '''
+    Generate a new network using the functions in the CityNetwork class. If a network already has been generated and stored in the data folder, comment this part and continue with PREPARE NETWORK. '''
 
     # Initialize CityNetwork object [N, S, E, W]
-    Delft = CityNetwork(name, coordinates, vehicle_type)
+    City = CityNetwork(name, coordinates, vehicle_type)
     
     # Load osm from local or online file
-    Delft.load_osm_graph(data_folder + name + '.osm')
-    Delft.load_building_addr(data_folder + name + '_building_addresses.csv', 
+    City.load_osm_graph(data_folder + name + '.osm')
+    City.load_building_addr(data_folder + name + '_building_addresses.csv', 
         data_folder + name + '_buildings.csv', 
         data_folder + name + '_addresses.csv')
     
-    print(Delft.building_addr_df)
+    print(City.building_addr_df)
 
     # Add speeds, lengths and distances to graph
     # Overwrite speed by using overwrite_bike=16
     # Further types available: overwrite_walk and overwrite_epv
-    Delft.add_rel_attributes(overwrite_bike=16)
+    City.add_rel_attributes(overwrite_bike=16)
 
     # Project graph
-    Delft.project_graph()
-    # Delft.plot()
+    City.project_graph()
+
+    # Plot the CityNetwork
+    # City.plot()
 
     # Calculate dataframes of nodes and edges
-    Delft.convert_graph_edges_to_df()
-    Delft.convert_graph_nodes_to_df()
+    City.convert_graph_edges_to_df()
+    City.convert_graph_nodes_to_df()
 
-    # # Save Pickle file
-    Delft.save_graph(name, data_folder)
+    # Save Pickle file
+    City.save_graph(name, data_folder)
     print('------------------------------------')
 
 
     ''' --- PREPARE NETWORK ---
     Load the network from .pkl file. Transform the origin and destination to the unprojected space.'''
-    # Load the Delft CityNetwork class object
-    Delft = CityNetwork.load_graph(name, data_folder)
+    # Load the CityNetwork class object
+    City = CityNetwork.load_graph(name, data_folder)
 
     # Specify the coordinates for origin and destination
     # Get the coordinates from building dataframe   
-    coordinates = Delft.building_addr_df.loc[:, ['latitude', 'longitude']]
+    coordinates = City.building_addr_df.loc[:, ['latitude', 'longitude']]
 
-    # Convert the coordinates to tuples, destinations are one goal
+    # Convert the coordinates to tuples, destinations are one goal, will be hubs
     origins = list(coordinates.itertuples(index=False, name=None))
     destinations = [((51.916328, 4.473386))] * len(origins)
 
-    # Extract the graph from the Delft CityNetwork
-    graph = Delft.graph
+    # Extract the graph from the City CityNetwork
+    graph = City.graph
     print(f"Processing {graph}")
 
     # Transform the start and origin to epsg:3857 (surface)
@@ -95,13 +97,13 @@ def main():
     It may be usefull to save the CityNetwork again after computing, using Delft.save_graph('...'), so that you can skip calculating the nearest edges. Remember however that the algorithm does not check if the coordinate inputs are the same as in the saved pickle file. ONLY do this if you are planning to reuse the nearest edges.
     The nearest edges from an earlier saved CityNetwork object can be removed using:
     
-    Delft.ne = None
-    Delft.save_graph('...')
+    City.ne = None
+    City.save_graph('...')
     '''
     print('------------------------------------')
 
     # Remove existing nearest_edges in CityNetwork object, proceed carefully:
-    # Delft.ne = None
+    # City.ne = None
 
     # Split the origins and destinations in x coordinate and y coordinate
     # Combine the origins and destinations values so they can be computed in one calculation
@@ -118,12 +120,13 @@ def main():
 
     # Find the nearest edges using multicore processing. If the CityNetwork class
     # already has nearest edges stored, it skips the computation.
+    # Number in input indicating the interpolation distance in meters
     
     print('Finding origin and destination edges...')
-    edges = Delft.nearest_edges(x, y, 5, cpus=None)
+    edges = City.nearest_edges(x, y, 5, cpus=None)
 
     # Save graph with edges, proceed carefully:
-    Delft.save_graph(name, data_folder)
+    City.save_graph(name, data_folder)
 
     # Split the found edges in origins and destinations (in half)
     orig_edge = edges[:len(edges)//2]
@@ -134,7 +137,7 @@ def main():
     Compute the shortest path between origin and destination, taking in account the position of the start and end in relation to the nearest edges. Input of orig, dest, orig_edge and dest_edge will interally be converted to a list, if inputed as tuples. '''    
     print('------------------------------------')
     start = time.time()
-    paths = Delft.shortest_paths(orig_yx_transf, dest_yx_transf, orig_edge, dest_edge, weight='travel_time', method='dijkstra', return_path=True, cpus=None)
+    paths = City.shortest_paths(orig_yx_transf, dest_yx_transf, orig_edge, dest_edge, weight='travel_time', method='dijkstra', return_path=True, cpus=None)
     end = time.time()
     print(f"Finished solving {len(paths)} paths in {round(end-start)}s.")
     # print(paths)
@@ -154,15 +157,15 @@ def main():
         edges.append((node, path[1][i+1], 0))
 
     # Specify which attributes to show
-    # Not sure which attributes are available? Use Delft.get_edge_attribute_types()
-    df = Delft.graph_edges_df.loc[edges, ['name', 'length', 'speed_kph', 'travel_time']]
+    # Not sure which attributes are available? Use City.get_edge_attribute_types()
+    df = City.graph_edges_df.loc[edges, ['name', 'length', 'speed_kph', 'travel_time']]
     print(df)
     
     ''' --- PLOT RESULTS ---
     Plot one or multiple paths using the CityNetwork plot functions. Advantage of this function is the ability to print multiple routes instead of one, including the linestrings calculated using the taxicab method.
     
     If you need different color and size settings for the plot, change them in the CityNetwork class (on top of class code)'''
-    fig, ax = Delft.plot(paths, orig_yx_transf, dest_yx_transf, save=True)
+    fig, ax = City.plot(paths, orig_yx_transf, dest_yx_transf, save=True)
     plt.show()
     
     return
